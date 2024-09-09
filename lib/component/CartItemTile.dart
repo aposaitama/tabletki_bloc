a0b35eed56cd514adcce98fe4a2629a8b5833cfe
apostolov <apostolov@knu.ua>
2024-09-09 17:25:14 +0300

    updated cartpage

--- lib/component/CartItemTile.dart
+++ lib/component/CartItemTile.dart
@@ -1,28 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tabletki/data/repository/medicines.dart';
+import 'package:tabletki/provider/medicines_provider.dart';
 
-class CartItemTile extends StatelessWidget {
+class CartItemTile extends StatefulWidget {
   final Datum medicine;
   const CartItemTile({super.key, required this.medicine});
 
   @override
+  State<CartItemTile> createState() => _CartItemTileState();
+}
+
+class _CartItemTileState extends State<CartItemTile> {
+  @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      color: Colors.white,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SizedBox(
-              width: 150, height: 150, child: Image.network(medicine.image)),
-          Column(
-            children: [
-              Text(medicine.name),
-              Text(medicine.price.toString()),
-            ],
+      height: 220,
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(8)),
+      margin: EdgeInsets.only(bottom: 10),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          leading: SizedBox(
+              width: 200,
+              height: 200,
+              child: Image.network(
+                widget.medicine.image,
+                fit: BoxFit.contain,
+              )),
+          title: Text(widget.medicine.name),
+          subtitle: Text(widget.medicine.price.toString()),
+          trailing: IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () => removeItemFromCart(),
           ),
-        ],
+        ),
       ),
     );
+  }
+
+  removeItemFromCart() {
+    Provider.of<MedicinesProvider>(context, listen: false)
+        .removeItemFromCart(widget.medicine);
   }
 }
